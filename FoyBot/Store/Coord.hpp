237893fc467ebb2328528a02e0c7502b6f20868d
Coac <levictor3000@hotmail.fr>
2017-01-03 19:36:38 +0100

    Added Coord  and Entity WIP

---
+++ FoyBot/Store/Coord.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+class Coord
+{
+private:
+	int x;
+	int y;
+public:
+	Coord(unsigned int const x1, unsigned int const y1);
+	unsigned int getX() const;
+	unsigned int getY() const;
+	void set(unsigned int x1, unsigned int y1);
+	void setX(unsigned int x1);
+	void setY(unsigned int y1);
+	void operator=(const Coord& c);
+	bool operator==(const Coord& a) const;
+
+};
+
+Coord::Coord(unsigned int x1, unsigned int y1)
+{
+	this->x = x1;
+	this->y = y1;
+}
+
+void Coord::set(unsigned int x1, unsigned int y1)
+{
+	this->x = x1;
+	this->y = y1;
+}
+
+void Coord::setX(unsigned int x1)
+{
+	this->x = x1;
+}
+
+void Coord::setY(unsigned int y1)
+{
+	this->y = y1;
+}
+
+unsigned int Coord::getX() const
+{
+	return x;
+}
+
+unsigned int Coord::getY() const
+{
+	return y;
+}
+
+void Coord::operator=(const Coord& c)
+{
+	x = c.getX();
+	y = c.getY();
+}
+
+bool Coord::operator==(const Coord& a) const
+{
+	return (getX() == a.getX() && getY() == a.getY());
+}
---
+++ FoyBot/Store/Entity.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Coord.hpp"
+#include <string>
+class Entity
+{
+private:
+	std::string name;
+	int id;
+	Coord pos;
+public:
+
+};
